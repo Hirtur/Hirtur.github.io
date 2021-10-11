a3127ef8929a7e0f6359df9ac229102259db95b1
Hirtur <54402940+Hirtur@users.noreply.github.com>
2021-10-11 13:47:19 +0000

    Add files via upload

---
+++ verkefni3vef3/countries.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, json, jsonify
+import urllib.request
+app = Flask(__name__)
+ 
+with urllib.request.urlopen("https://restcountries.eu/rest/v2/all") as url:
+    gogn = json.loads(url.read().decode())
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', lf = gogn)
+
+@app.route('/land/<alpha2Code>')
+def land(alpha2Code):
+    for i in gogn:
+        if alpha2Code == i['alpha2Code']:
+            land = i
+            break
+    return render_template('land.html', land=land)
+
+@app.route('/heimsalfur/<region>')
+def heimsalfur(region):
+    fjoldi = 0
+    for i in gogn:
+        if i['region'] == region:
+            fjoldi += 1
+    return render_template('heimsalfur.html', lf = gogn, region = region, fjoldi = fjoldi)
+
+if __name__ == "__main__":
+    app.run(debug=True)
